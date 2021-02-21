4410cbedebeeff11f76228d843d668e295cca026
MHDante <hello@mhdante.com>
2021-02-21 04:29:48 -0500

    added submodule and setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name="flowtron-module",
+    version="0.1",
+    description="Pipelining to allow using flowtron as a pip module",
+    author="NVIDIA",
+    author_email="rafaelvalle@nvidia.com",
+    url="https://transforms.ai/",
+    package_dir={"": "src"},
+    packages=["flowtron"],
+    install_requires=[
+        "torch==1.7.1"
+        "matplotlib==3.3.2",
+        "numpy==1.19.2",
+        "inflect==4.1.0",
+        "librosa==0.6.3",
+        "scipy==1.5.2",
+        "Unidecode==1.0.22",
+        "torch==1.7.1",
+        "pillow",
+        "tensorboardX",
+    ],
+    license="Apache-2.0 License",
+)

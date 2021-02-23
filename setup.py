a59b2121eea3f0bf697471127e4610a0e92d7a7c
MHDante <hello@mhdante.com>
2021-02-23 02:59:57 -0500

    make init files before setup

--- setup.py
+++ setup.py
@@ -1,4 +1,43 @@
 from distutils.core import setup
+import os
+
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+class PreDevelopCommand(develop):
+    """Pre-installation for development mode."""
+
+    def run(self):
+        make_inits()
+        develop.run(self)
+
+
+class PreInstallCommand(install):
+    """Pre-installation for installation mode."""
+
+    def run(self):
+        make_inits()
+        install.run(self)
+
+
+def make_inits():
+    currentDir = os.path.dirname(__file__)
+    touch(os.path.join(currentDir, "cheem"))
+    touch(os.path.join(currentDir, "./src/flowtron/__init__.py"))
+    touch(os.path.join(currentDir, "./src/flowtron/tacotron2/__init__.py"))
+    touch(os.path.join(currentDir, "./src/flowtron/tacotron2/waveglow/__init__.py"))
+
+
+def touch(path: str):
+    basedir = os.path.dirname(path)
+    if not os.path.exists(basedir):
+        os.makedirs(basedir)
+
+    with open(path, "a"):
+        os.utime(path, None)
+
 
 setup(
     name="flowtron-module",
@@ -7,7 +46,11 @@ setup(
     author="NVIDIA",
     author_email="rafaelvalle@nvidia.com",
     package_dir={"": "src"},
-    packages=["flowtron","flowtron.tacotron2","flowtron.tacotron2.waveglow"],
+    packages=["flowtron", "flowtron.tacotron2", "flowtron.tacotron2.waveglow"],
+    cmdclass={
+        "develop": PreDevelopCommand,
+        "install": PreInstallCommand,
+    },
     install_requires=[
         "torch==1.7.1",
         "matplotlib==3.3.2",
